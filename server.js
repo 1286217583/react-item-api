cd6a0fa5aafcad9adeed7f99137ad2c359fbe541
1286217583 <1286217583@qq.com>
2021-05-11 02:05:56 +0800

    init

---
+++ server.js
@@ -0,0 +1,14 @@
+
+const jsonServer = require('json-server')
+
+const routes = jsonServer.router('./da.json')
+const middlewares = jsonServer.defaults()
+
+const server = jsonServer.create()
+
+
+// json-server 假数据
+server.use(middlewares)
+server.use(routes)
+
+server.listen(9090)
